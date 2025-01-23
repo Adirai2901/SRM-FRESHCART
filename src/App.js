c5991dce19d32dc93806a7922a79d8a110345e53
Aditya Rai <adirai2901@gmail.com>
2025-01-23 14:25:42 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -10,28 +10,12 @@ import Header from './Component/Header';
 import Home from "./pages/Home";
 // About pages
 import AboutUs from "./pages/About/AboutUs";
-import Blog from "./pages/About/Blog";
-import BlogCategory from "./pages/About/BlogCategory";
+import Blog from "./pages/About/Blog"; 
 import Contact from "./pages/About/Contact";
-// Shop pages
-// import Shop from "./pages/Shop/Shop";
-// import ShopGridCol3 from "./pages/Shop/ShopGridCol3";
-// import ShopListCol from "./pages/Shop/ShopListCol";
-// // import ShopCart from "./pages/Shop/ShopCart";
-// import ShopCheckOut from "./pages/Shop/ShopCheckOut";
-// import ShopWishList from "./pages/Shop/ShopWishList";
-// Store pages
+ 
 import StoreList from "./pages/store/StoreList";
 import SingleShop from "./pages/store/SingleShop";
-// Account pages
-import MyAccountOrder from "./pages/Accounts/MyAccountOrder";
-import MyAccountSetting from "./pages/Accounts/MyAcconutSetting";
-import MyAcconutNotification from "./pages/Accounts/MyAcconutNotification";
-import MyAcconutPaymentMethod from "./pages/Accounts/MyAcconutPaymentMethod";
-import MyAccountAddress from "./pages/Accounts/MyAccountAddress";
-import MyAccountForgetPassword from "./pages/Accounts/MyAccountForgetPassword";
-import MyAccountSignIn from "./pages/Accounts/MyAccountSignIn";
-import MyAccountSignUp from "./pages/Accounts/MyAccountSignUp";
+ 
 const App = () => {
   return (
     <div>
@@ -39,28 +23,12 @@ const App = () => {
         <Header/>
         <Routes>
           <Route path="/Grocery-react/" element={<Home />} />
-          {/* Shop pages */}
-          {/* <Route path="/Shop" element={<Shop />} />
-          <Route path="/ShopGridCol3" element={<ShopGridCol3 />} />
-          <Route path="/ShopListCol" element={<ShopListCol />} />
-          <Route path="/ShopWishList" element={<ShopWishList />} />
-          <Route path="/ShopCheckOut" element={<ShopCheckOut />} /> */}
-          {/* <Route path="/ShopCart" element={<ShopCart />} /> */}
-          {/* Store pages */}
+          
           <Route path="/StoreList" element={<StoreList />} />
           <Route path="/SingleShop" element={<SingleShop />} />
-          {/* Accounts pages */}
-          <Route path="/MyAccountOrder" element={<MyAccountOrder />} />
-          <Route path="/MyAccountSetting" element={<MyAccountSetting />} />
-          <Route path="/MyAcconutNotification" element={<MyAcconutNotification />} />
-          <Route path="/MyAcconutPaymentMethod" element={<MyAcconutPaymentMethod />} />
-          <Route path="/MyAccountAddress" element={<MyAccountAddress />} />
-          <Route path="/MyAccountForgetPassword" element={<MyAccountForgetPassword />} />
-          <Route path="/MyAccountSignIn" element={<MyAccountSignIn />} />
-          <Route path="/MyAccountSignUp" element={<MyAccountSignUp />} />
+       
           {/* About pages */}
-          <Route path="/Blog" element={<Blog />} />
-          <Route path="/BlogCategory" element={<BlogCategory />} />
+          <Route path="/Blog" element={<Blog />} /> 
           <Route path="/Contact" element={<Contact />} />
           <Route path="/AboutUs" element={<AboutUs />} />
         </Routes>
